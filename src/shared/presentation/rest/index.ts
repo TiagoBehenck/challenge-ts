ab95eb01db89fb4585daac3f0a2f58a9fb0a635e
Tiago Behenck <tiago.behenck@cwi.com.br>
2023-11-29 14:47:12 -0300

    move the listen port to shared presentation

--- src/shared/presentation/rest/index.ts
+++ src/shared/presentation/rest/index.ts
@@ -1,8 +1,18 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+
 import { Config } from '../../../index'
 import { restLayerStudent } from '../../../student/presentation/index'
 
+const app = express()
+const PORT = process.env.PORT || 3000
+
 const start = (config: Config) => {
+  app.use(bodyParser.json())
+  
   restLayerStudent(config)
+
+  app.listen(config.port, () => console.log(`Listening on port ${PORT}`))
 }
 
 export { start }
