954d473edd71ac54b7ee6846e9a4dd7d1e6bc12b
Tiago Behenck dos Santos <tiago.behenck.santos@gmail.com>
2023-12-03 18:48:11 -0300

    create teacher service

---
+++ src/services/TeacherService.ts
@@ -0,0 +1,30 @@
+import { Teacher, TeacherCreationType, TeacherUpdateType } from '../domain/Teacher.js'
+import { ConflictError } from '../domain/errors/ConflictError.js'
+import { Service } from './BaseService.js'
+
+export class TeacherService extends Service { 
+  update(id: string, newData: TeacherUpdateType) { 
+    const entity = this.findById(id) as Teacher
+    
+    const updated = new Teacher({
+      ...entity.toObject(),
+      ...newData,
+    })
+
+    this.repository.save(updated)
+    
+    return updated
+  }
+
+  create(creationData: TeacherCreationType) { 
+    const existing = this.repository.listBy('document', creationData.document)
+
+    if (existing.length > 0) throw new ConflictError(creationData.document, this.repository.dbEntity);
+    
+    const entity = new Teacher(creationData)
+
+    this.repository.save(entity)
+
+    return entity
+  }
+}
